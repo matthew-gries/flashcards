c8e1f8da69ba6db0eabb8e72e884bdb0d6827c1b
matthew-gries <gries.m@husky.neu.edu>
2021-07-27 23:21:30 -0400

    made improvements to resizing

--- flashcards/main.py
+++ flashcards/main.py
@@ -12,35 +12,39 @@ class Application(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
         self.master = master
-        self.pack()
         self.create_widgets()
+        self.pack(expand=True)
         self.words = None
         self.current_word = None
         self.current_definition = None
 
     def create_widgets(self):
-        self.grid(row=0, column=0, sticky="we")
 
         self.load_words_button = tk.Button(self)
         self.load_words_button["text"] = "Load words"
         self.load_words_button["command"] = self.load_words_func
-        self.load_words_button.grid(row=0, column=0, padx=10, pady=10)
+        self.load_words_button.grid(row=0, column=0, padx=10, pady=10, sticky='nsew')
 
         self.next_button = tk.Button(self)
         self.next_button["text"] = "=>"
         self.next_button["state"] = "disabled"
         self.next_button["command"] = self.get_random_word_and_definition
-        self.next_button.grid(row=0, column=1, padx=10, pady=10)
+        self.next_button.grid(row=0, column=1, padx=10, pady=10, sticky='nsew')
     
         self.quit = tk.Button(self, text="QUIT", fg="red", command=self.master.destroy)
-        self.quit.grid(row=0, column=2, padx=10, pady=10)
+        self.quit.grid(row=0, column=2, padx=10, pady=10, sticky='nsew')
 
-
-        self.flashcard = tk.Button(self, width=50, height=10)
+        self.flashcard = tk.Button(self, width=100, height=30)
         self.flashcard["text"] = "Load words to continue..."
         self.flashcard["state"] = "disabled"
         self.flashcard["command"] = self.flip_text
-        self.flashcard.grid(row=1, column=0, columnspan=3, padx=10, pady=10, sticky="ewns")
+        self.flashcard.grid(row=1, column=0, columnspan=3, padx=10, pady=10, sticky='nsew')
+
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_columnconfigure(1, weight=1)
+        self.grid_columnconfigure(2, weight=1)
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_rowconfigure(1, weight=1)
 
     def load_words_func(self):
         filename = fd.askopenfilename()
@@ -73,7 +77,6 @@ class Application(tk.Frame):
 
 root = tk.Tk()
 root.title("Flashcards")
-root.columnconfigure(0, weight=1)
-root.rowconfigure(1, weight=1)
+root.geometry("960x640")
 app = Application(master=root)
 app.mainloop()
